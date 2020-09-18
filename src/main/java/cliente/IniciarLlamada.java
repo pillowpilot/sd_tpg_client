e2e130fe2a2cff210eb6c779dc969b88ed4ca2eb
modesto <modesto@outlook.com>
2020-09-18 18:25:22 -0400

    inicar llamada

---
+++ src/main/java/cliente/IniciarLlamada.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+
+import cliente.models.MensajeLlamada;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author modesto
+ */
+public class IniciarLlamada implements Runnable{
+    String ip;
+    int puerto;
+    String emisor;
+    String receptor;
+    @Override
+    public void run() {
+        
+        
+        Socket s; 
+        try {
+            
+            
+            ObjectMapper jsonMapper = new ObjectMapper();
+            
+            MensajeLlamada mensaje = new MensajeLlamada("-1","ok","2",this.emisor,this.receptor,"") ;
+            
+            s = new Socket(ip, this.puerto);
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+             
+             String envio = jsonMapper.writeValueAsString(mensaje);
+             
+            out.println(envio);
+            
+            
+            String recibo = in.readLine();
+            mensaje=jsonMapper.readValue(recibo, MensajeLlamada.class);
+            
+            //0 Significa que se acpeta la llamada
+            if(mensaje.getEstado()=="0"){
+                Llamada conversacion = new Llamada(true,s,this.emisor,this.receptor);
+                conversacion.run();
+                
+            }else{
+                    System.out.println("Llamada rechazada o cliente receptor ocupado");
+            
+            }
+            out.close();
+            in.close();
+             
+        } catch (IOException ex) {
+            Logger.getLogger(IniciarLlamada.class.getName()).log(Level.SEVERE, null, ex);
+        }
+          
+        // obtaining input and out streams 
+       
+    }
+    
+    
+}
--- src/main/java/cliente/Llamada.java
+++ src/main/java/cliente/Llamada.java
@@ -9,7 +9,11 @@ package cliente;
 import cliente.models.MensajeLlamada;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
@@ -22,71 +26,167 @@ import java.util.logging.Logger;
  */
 public class Llamada implements Runnable{
     private boolean On_going_call;
+    private Socket S;
+    private String emisor;
+    private String receptor;
+
+    public Llamada(boolean On_going_call, Socket S, String emisor, String receptor) {
+        this.On_going_call = On_going_call;
+        this.S = S;
+        this.emisor = emisor;
+        this.receptor = receptor;
+    }
+
+    public Llamada() {
+    }
+
+
+ 
     
     
     public static void main(String a[]) throws Exception {
        
     
     }
+
+    public void setEmisor(String emisor) {
+        this.emisor = emisor;
+    }
+
+    public void setReceptor(String receptor) {
+        this.receptor = receptor;
+    }
+
+    public void setOn_going_call(boolean On_going_call) {
+        this.On_going_call = On_going_call;
+    }
+
+    public void setS(Socket S) {
+        this.S = S;
+    }
+
+    public boolean isOn_going_call() {
+        return On_going_call;
+    }
+
+    public Socket getS() {
+        return S;
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public String getReceptor() {
+        return receptor;
+    }
       @Override
     public void run() {
-        boolean Call_On_going=true;
-        BlockingQueue<String> pendingMessagesToSend = null;
-        BlockingQueue<String> pendingMessagesToReceive = null;
-        ObjectMapper jsonMapper = new ObjectMapper();
-        TCPConnectionManager Conexion;
         
-        Conexion = new TCPConnectionManager("127.1.0.0", Integer.SIZE, pendingMessagesToSend, pendingMessagesToReceive);
-        Conexion.run();
-        
+        ObjectMapper jsonMapper = new ObjectMapper();     
+                       
         Scanner scn = new Scanner(System.in);
-        String mnsjleido;
-        
-        MensajeLlamada mensajeR = new MensajeLlamada();
-        
+            
+
+          
+
         MensajeLlamada mensajeE = new MensajeLlamada();
-         mensajeE.setMensaje("ok");
-         mensajeE.setTipo_operacion("5");
-         mensajeE.setEstado("0");
-        while(On_going_call){
-            mnsjleido = scn.nextLine();
+        mensajeE.setEmisor(this.emisor);
+        mensajeE.setReceptor(this.receptor);
+        mensajeE.setMensaje("ok");
+        mensajeE.setTipo_operacion("3");
+        mensajeE.setEstado("0");
+
             
-            
-            mensajeE.setTexto(mnsjleido);
-           
-            
-            try {
-                String envia = jsonMapper.writeValueAsString(mensajeE);
-                pendingMessagesToSend.add(envia);
-            } catch (JsonProcessingException ex) {
-                Logger.getLogger(Llamada.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            if(pendingMessagesToReceive!=null){
+                
+           Thread sendMessage = new Thread(new Runnable(){ 
+            @Override
+            public void run() { 
+                String mnsjleido;
+                 PrintWriter out;
                 try {
-                    String recibio = pendingMessagesToReceive.take();
-                    mensajeR = jsonMapper.readValue(recibio, MensajeLlamada.class);
-                    
-                    
-                    if(mensajeR.getTipo_operacion()=="4"){
-                         System.out.println("Se ha cortado la llamada");    
-                         this.stopCall();
-                    }else{
-                        System.out.println(mensajeR.getTexto());
-                    
-                    }
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Llamada.class.getName()).log(Level.SEVERE, null, ex);
+                    out = new PrintWriter(S.getOutputStream(), true);
+                    while (On_going_call) { 
+                        
+                        mnsjleido = scn.nextLine();
+                        if(mnsjleido =="bye"){
+                            mensajeE.setTipo_operacion("4");
+                        }
+                        mensajeE.setTexto(mnsjleido);
+                        
+                                       
+                        try { 
+
+                          String envia = jsonMapper.writeValueAsString(mensajeE);
+                          out.println(envia);
+                        } catch (IOException e) { 
+                            e.printStackTrace(); 
+                        } 
+                    } 
+                out.close();
                 } catch (IOException ex) {
                     Logger.getLogger(Llamada.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 
-              
+            } 
+        }); 
                 
-            }
+             
+             Thread readMessage = new Thread(new Runnable()  
+        { 
+            @Override
+            public void run() { 
+                BufferedReader in = null;
+                try {
+                    MensajeLlamada mensajeR = new MensajeLlamada();
+                    in = new BufferedReader(new InputStreamReader(S.getInputStream()));
+                    while (On_going_call) {
+                        try {
+                            
+                            String recibio = in.readLine();
+                            mensajeR = jsonMapper.readValue(recibio, MensajeLlamada.class);
+                            if(mensajeR.getTipo_operacion()=="4"){
+                                System.out.println("bye....Se ha cortado la llamada");
+                                On_going_call=false;
+                            }else{
+                                System.out.println(mensajeR.getTexto());
+                                
+                            }
+                        } catch (IOException e) {
+                            
+                            e.printStackTrace();
+                        } 
+                    }   in.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Llamada.class.getName()).log(Level.SEVERE, null, ex);
+                } 
+            } 
+        }); 
+               
+                
+                
+                
+                
+              
+            sendMessage.start();
+            readMessage.start();
+       
+           
+            
+            
+       
+       
             
         }
-    }
-    private void stopCall(){
+     private void stopCall(){
         this.On_going_call=false;
+        try {
+            this.S.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Llamada.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
-}
+    }
+   
+
